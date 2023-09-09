aff7dc837b7a6b20a92266af172cdfabbb37e6e3
Deyvson <deyvsonsoundguitar19@gmail.com>
2023-09-09 18:39:55 -0300

    atualizações

--- mundo_2/desafio48.py
+++ mundo_2/desafio48.py
@@ -2,8 +2,9 @@
  que são múltiplos de três e que se encontram no intervalo de 1 até 500. '''
 
 soma = 0
+cont = 0
 for num in range(1, 501, 2):
     if num % 3 == 0:
         soma = soma + num
         cont = cont + 1
-print(f'A soma de todos os valores solicitados é {soma}')
+print(f'A soma de todos os valores que é {cont} solicitados é {soma}')
