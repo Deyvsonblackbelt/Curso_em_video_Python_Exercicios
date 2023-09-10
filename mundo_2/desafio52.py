c138b350a34f34adf9348dba5b5fb8379252691d
Deyvson <deyvsonsoundguitar19@gmail.com>
2023-09-10 10:04:48 -0300

    atualizações

---
+++ mundo_2/desafio52.py
@@ -0,0 +1,19 @@
+'''Exercício Python 52: Faça um programa que leia um número inteiro
+e diga se ele é ou não um número primo.'''
+
+tot = 0
+num = int(input('Digite um número: '))
+for c in range(1, num + 1):
+    if num % c == 0:
+        print('\033[33m', end='')
+        tot += 1
+    else:
+        print('\033[31m', end='')
+    print(f'{c}', end='')
+print(f'\n\033[m O número {num} foi divisível {tot} vezes')
+if tot == 2:
+    print('E por isso ele é primo!')
+else:
+    print('E por isso ele não é primo!')
+
+
